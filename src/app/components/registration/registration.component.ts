fc8afbf3cd59fc697235181bd5021bdce3ccf9a8
Shahmy Abdulla <abdulla@dxdy.tech>
2023-05-12 22:57:34 +0530

    registration changes

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { RegistrationService } from '../../registration.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,8 +10,37 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+  registrationForm = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl('')
+  });
+
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  password: string = '';
+  confirm_password: string = '';
+
+  constructor(private getRegistrationService: RegistrationService) {
+  }
+
   userRegistrtion() {
-    
+
+    console.log(this.registrationForm.value);
+
+    this.getRegistrationService.
+    userRegistration(this.first_name, this.last_name, this.email, this.password, this.confirm_password)
+    .subscribe((user: any) => {
+
+      if (user) {
+        alert('Registration Successful');
+      }
+
+    });
+
   }
 
 }
--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -8,12 +8,13 @@ export class RegistrationService {
 
   constructor(private http: HttpClient) { }
 
-  register(first_name: string, last_name:string, email: string, password: string) {
+  userRegistration(first_name: string, last_name:string, email: string, password: string, confirm_password: string) {
     const body = {
       first_name,
       last_name,
       email,
-      password
+      password,
+      confirm_password
     };
     return this.http.post('http://127.0.0.1:8000/api/user-registration', body);
   }
